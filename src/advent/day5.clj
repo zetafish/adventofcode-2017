33f6c0528f7175ccb5fbfc3f63b5173f2c835081
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-05 21:06:59 +0100

    Day 5 - part 2

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -8,24 +8,31 @@
        (str/split-lines)
        (mapv #(Integer/parseInt %))))
 
-(defn step
-  [{:keys [jumps pos]}]
-  (if (or (nil? pos)
-          (neg? pos)
-          (>= pos (count jumps)))
-    nil
-    {:pos (+ pos (get jumps pos))
-     :jumps (update jumps pos inc)}))
+(defn make-stepper
+  [updater]
+  (fn [{:keys [jumps pos]}]
+    (if (or (nil? pos)
+            (neg? pos)
+            (>= pos (count jumps)))
+      nil
+      {:pos (+ pos (get jumps pos))
+       :jumps (update jumps pos updater)})))
 
-(defn step-all
-  [jumps]
-  (->> {:jumps jumps :pos 0}
-       (iterate step)
-       (take-while #(not (nil? %)))))
+(defn make-jumper
+  [stepper]
+  (fn [jumps]
+    (->> {:jumps jumps :pos 0}
+         (iterate stepper)
+         (take-while #(not (nil? %))))))
 
-(defn compute-1
-  [jumps]
-  (dec (count (step-all jumps))))
+(defn compute
+  [jumper jumps]
+  (dec (count (jumper jumps))))
 
-#_ (compute-1 [0 3 0 1 -3])
-#_ (compute-1 input)
+#_ (-> (make-stepper inc)
+       (make-jumper)
+       (compute input))
+
+#_ (-> (make-stepper #(if (>= % 3) (dec %) (inc %)))
+       (make-jumper)
+       (compute input))

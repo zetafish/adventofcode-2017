7dad1172ce4605304dc41a59b171d1691aad833b
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-09 22:53:22 +0100

    Day 7

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -1,14 +1,8 @@
 (ns advent.day7
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.set :as set]))
-
-(def pat #"(\w+) \((\d+)\)( -> (\w+)(, \w+)*)?")
-
-(def s1 "pgvds (134) -> manpd, vkikri")
-(def s2 "asdad (12)")
-
-(re-matches pat s1)
+            [clojure.set :as set]
+            [clojure.walk :as walk]))
 
 (defn parse-line
   [s]
@@ -22,12 +16,72 @@
   [programs]
   (let [names (map :name programs)
         non-bottom (mapcat :above programs)]
-    (set/difference (set names) (set non-bottom))))
-
+    (first (set/difference (set names) (set non-bottom)))))
 
 (def input (->> (io/resource "input7.txt")
                 (slurp)
                 (str/split-lines)
                 (map parse-line)))
 
-(find-bottom input)
+(def nodes (->> input
+                (map (juxt :name identity))
+                (into {})))
+
+(defn above
+  [n]
+  (-> n nodes :above))
+
+(defn weight
+  [n]
+  (-> n nodes :weight))
+
+
+(defn total
+  [n]
+  (reduce +
+          (weight n)
+          (map total (above n))))
+
+(defn find-bad
+  [n]
+  (let [a (above n)
+        t (map total a)
+        v (->> (frequencies t)
+               (filter #(= 1 (second %)))
+               (first)
+               (first))]
+    (->> (interleave a t)
+         (partition 2)
+         (filter #(= v (second %)))
+         (first))))
+
+(defn balanced?
+  [n]
+  (= 1 (->> (above n)
+          (map total)
+          (frequencies)
+          (count))))
+
+(defn find-fix
+  [n]
+  (let [a (above n)
+        t (map total a)
+        x (->> (interleave t a)
+               (partition 2)
+               (map vec)
+               (into {}))
+        f (frequencies t)
+        good-value (first (first (filter #(not= 1 (second %)) f)))
+        bad-value (first (first (filter #(= 1 (second %)) f)))
+        to-add (- good-value bad-value)]
+    [(x bad-value) to-add]))
+
+
+(defn fix-tree
+  [node k]
+  (if (balanced? node)
+    (+ k (weight node))
+    (let [[n2 k2] (find-fix node)]
+      (recur n2 k2))))
+
+(fix-tree bottom 0)

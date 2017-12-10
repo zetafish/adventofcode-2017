1adda96242aa5b78e3b8a2edfb1f77cc7798d665
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-10 01:44:39 +0100

    update

--- src/advent/day1.clj
+++ src/advent/day1.clj
@@ -4,37 +4,30 @@
 
 (def input (str/trim (slurp (io/resource "input1.txt"))))
 
-(defn ch->int
-  [ch]
-  (- (int ch) (int \0)))
+(defn ->digits
+  [s]
+  (map #(- (int %) (int \0)) s))
+
+(defn rotate
+  [n coll]
+  (concat (drop n coll) (take n coll)))
 
 (defn compute
   [n s]
-  (let [c (map ch->int s)
-        pairs (map vector c (concat (drop n c)
-                                    (take n c)))]
-    (->> pairs
-         (filter (fn [[a b]] (= a b)))
-         (map first)
-         (reduce +))))
+  (let [d (->digits s)]
+    (->>
+      (map vector d (rotate n d))
+      (filter (fn [[a b]] (= a b)))
+      (map first)
+      (reduce +))))
 
-(defn compute-1
+(defn part-1
   [s]
   (compute 1 s))
 
-(defn compute-n
+(defn part-2
   [s]
   (compute (/ (count s) 2) s))
 
-(compute-1 "1122")
-(compute-1 "1111")
-(compute-1 "1234")
-(compute-1 "91212129")
-(compute-1 input)
-
-(compute-n "1212")
-(compute-n "1221")
-(compute-n "123425")
-(compute-n "123123")
-(compute-n "12131415")
-(compute-n input)
+#_ (part-1 input)
+#_ (part-2 input)

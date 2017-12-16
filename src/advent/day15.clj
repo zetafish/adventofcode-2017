209ae5f6258b5d6b54b8cb29ccd369fdcb73286f
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-16 02:44:19 +0100

    Day 15 - some refactoring

--- src/advent/day15.clj
+++ src/advent/day15.clj
@@ -2,19 +2,12 @@
 
 ;; Duelling Generators
 
-;; The generators both work on the same principle. To create its next
-;; value, a generator will take the previous value it produced,
-;; multiply it by a factor (generator A uses 16807; generator B uses
-;; 48271), and then keep the remainder of dividing that resulting
-;; product by 2147483647. That final remainder is the value it
-;; produces next.
-
 (def magic-constant 2147483647)
 
 (defn generator
   [factor]
   (fn [previous]
-    (mod (* previous factor)
+    (rem (* previous factor)
          magic-constant)))
 
 (defn last-16-bits
@@ -34,10 +27,10 @@
     (map last-16-bits)))
 
 (defn compute
-  [c1 c2 pairs]
-  (->> (map vector c1 c2)
-       (take pairs)
-       (filter (fn [[x y]] (= x y)))
+  [c1 c2 n]
+  (->> (map = c1 c2)
+       (take n)
+       (remove false?)
        (count)))
 
 ;; Part 1
@@ -53,8 +46,8 @@
 ;; Part 2
 (comment
   (compute (gen a 65 4)
-           (gen b 8921 8)
-           (* 5 1000 1000))
+          (gen b 8921 8)
+          (* 5 1000 1000))
 
   (compute (gen a 512 4)
            (gen b 191 8)

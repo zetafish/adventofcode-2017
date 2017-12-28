1d19bef5fcc7d18631261d93e212b9110443627c
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-28 20:57:02 +0100

    Day 22 - part 2

--- src/advent/day22.clj
+++ src/advent/day22.clj
@@ -4,51 +4,73 @@
 
 ;; --- Day 22: Sporifica Virus ---
 
-(def turn-right
-  {[1 0] [0 -1]
-   [0 -1] [-1 0]
-   [-1 0] [0 1]
-   [0 1] [1 0]})
+(def lifecycle
+  {:clean :weakened
+   :weakened :infected
+   :infected :flagged
+   :flagged :clean})
 
-(def turn-left
-  {[1 0] [0 1]
-   [0 1] [-1 0]
-   [-1 0] [0 -1]
-   [0 -1] [1 0]})
+(defn turn-right
+  [[x y]]
+  [y (- x)])
 
-(defn make-turn
+(defn turn-left
+  [[x y]]
+  [(- y) x])
+
+(defn reverse-dir
+  [[x y]]
+  [(- x) (- y)])
+
+(defn turn
   [{:keys [grid pos dir] :as state}]
-  (if (grid pos)
-    (update state :dir turn-right)
-    (update state :dir turn-left)))
+  (condp = (grid pos :clean)
+    :infected (update state :dir turn-right)
+    :clean (update state :dir turn-left)
+    :weakened state
+    :flagged (update state :dir reverse-dir)))
 
-(defn toggle-infection
-  [{:keys [grid pos] :as state}]
-  (if (grid pos)
-    (update state :grid disj pos)
+(defn evolve
+  [m {:keys [grid pos] :as state}]
+  (let [v (grid pos :clean)]
     (-> state
-        (update :grid conj pos)
-        (update :infections inc))))
+        (update :grid assoc pos (m v))
+        (update v inc))))
 
-(defn move-forward
+(defn move
   [{:keys [grid pos dir] :as state}]
   (update state :pos #(mapv + % dir)))
 
 (defn step
-  [{:keys [grid pos dir] :as state}]
-  (-> state
-      make-turn
-      toggle-infection
-      move-forward))
+  [m {:keys [grid pos dir] :as state}]
+  (->> state
+       (turn)
+       (evolve m)
+       (move)))
+
+(defn make-grid
+  [g]
+  (let [n (count g)
+        g (vec (reverse g))]
+    (into {}
+          (for [x (range n)
+                y (range n)
+                :when (= \# (get-in g [y x]))]
+            [[(- x (quot n 2)) (- y (quot n 2))]
+             :infected]))))
 
 (defn draw
   [n {:keys [grid pos dir] :as state}]
-  (letfn [(cell [p] (let [c (if (grid p) "#" ".")]
+  (letfn [(cell [p] (let [c (condp = (grid p :clean)
+                              :infected \#
+                              :clean \.
+                              :weakened \W
+                              :flagged \F)]
                       (cond
                           (= p pos) (str "[" c "]")
                           (= p (mapv + pos dir)) (str "(" c ")")
                           :else (str " " c " "))))]
-    (println state)
+    (println (dissoc state :grid))
     (println
       (str/join "\n"
                 (for [y (range n (dec (- n)) -1)]
@@ -56,19 +78,33 @@
                     (for [x (range (- n) (inc n))]
                       (cell [x y]))))))))
 
-(defn simulate
+(def toggle {:clean :infected
+             :infected :clean})
+
+(def evolution {:clean :weakened
+                :weakened :infected
+                :infected :flagged
+                :flagged :clean})
+
+(defn simulate-1
   [n seed]
-  (->> (iterate step seed)
+  (->> (iterate (partial step toggle) seed)
        (drop n)
        (first)))
 
-(make-grid (mapv vec ["..#" "#.." "..."]))
+(defn simulate-2
+  [n seed]
+  (->> (iterate (partial step evolution) seed)
+       (drop n)
+       (first)))
+
+(def simulate simulate-2)
 
 (def seed
   {:grid (make-grid (mapv vec ["..#" "#.." "..."]))
    :pos [0 0]
    :dir [0 1]
-   :infections 0})
+   :infected 0 :clean 0 :flagged 0 :weakened 0})
 
 
 (draw 4 (simulate 0 seed))
@@ -78,31 +114,21 @@
 (draw 4 (simulate 4 seed))
 (draw 4 (simulate 5 seed))
 (draw 4 (simulate 6 seed))
-
 (draw 4 (simulate 7 seed))
-(draw 4 (simulate 70 seed))
+(draw 4 (simulate 100 seed))
 (draw 4 (simulate 10000 seed))
+(draw 4 (simulate 10000000 seed))
 
 (def input (->> (io/resource "input22.txt")
                 (slurp)
                 (str/split-lines)
                 (mapv vec)))
 
-(defn make-grid
-  [g]
-  (let [n (count g)
-        g (vec (reverse g))]
-    (set
-          (for [x (range n)
-                y (range n)
-                :when (= \# (get-in g [y x]))]
-            [(- x (quot n 2)) (- y (quot n 2))]))))
-
 
 (def seed
   {:grid (make-grid input)
    :pos [0 0]
    :dir [0 1]
-   :infections 0})
+   :infected 0 :clean 0 :flagged 0 :weakened 0})
 
-(simulate 10000 seed)
+(println (dissoc (simulate 10000000 seed) :grid))

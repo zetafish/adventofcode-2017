a8bbfcd762e5e61cd17aaff713b2c05f7cd22f3a
Endymion Kasanardjo <endy.kasanardjo@greenhousegroup.com>
2017-12-29 12:23:39 +0100

    Day 23 - part 2

--- src/advent/day23.clj
+++ src/advent/day23.clj
@@ -5,11 +5,12 @@
 
 ;; --- Day 23: Coprocessor Conflagration ---
 (def grammar
-  "S = set | sub | mul | jnz
+  "S = (set | sub | mul | jnz | mod) <#'.*'>
   set = <'set '> var <' '> (var | num)
   sub = <'sub '> var <' '> (var | num)
   mul = <'mul '> var <' '> (var | num)
   jnz = <'jnz '> (var | num) <' '> (var | num)
+  mod = <'mod '> var <' '> (var | num)
   var = #'[a-z]'
   num = #'-?[0-9]+'")
 
@@ -24,9 +25,11 @@
   [s]
   (insta/transform options (insta/parse parser s)))
 
-(def code (mapv parse (-> (io/resource "input23.txt")
-                          (slurp)
-                          (str/split-lines))))
+(defn read-code
+  []
+  (mapv parse (-> (io/resource "input23.txt")
+                  (slurp)
+                  (str/split-lines))))
 
 (defn safe-inc
   [v]
@@ -40,22 +43,39 @@
 
 (defn set-op
   [state a b]
-  (-> state
-      (assoc-in [:var a] (value state b))
-      (update :ip inc)))
+  (let [v (value state b)]
+    (-> state
+        (assoc-in [:var a] v)
+        (update :ip inc))))
 
 (defn sub-op
   [state a b]
-  (-> state
-      (assoc-in [:var a] (- (value state a) (value state b)))
-      (update :ip inc)))
+  (let [v (- (value state a) (value state b))]
+    (-> state
+        (assoc-in [:var a] v)
+        (update :ip inc))))
 
 (defn mul-op
   [state a b]
-  (-> state
-      (assoc-in [:var a] (* (value state a) (value state b)))
-      (update :mul safe-inc)
-      (update :ip inc)))
+  (let [v (* (value state a) (value state b))]
+    (-> state
+        (assoc-in [:var a] v)
+        (update :mul safe-inc)
+        (update :ip inc))))
+
+(defn mod-op
+  [state a b]
+  (let [v (rem (value state a) (value state b))]
+    (-> state
+        (assoc-in [:var a] v)
+        (update :ip inc))))
+
+(defn sqrt-op
+  [state a b]
+  (let [v (int (Math/sqrt (value state b)))]
+    (-> state
+        (assoc-in [:var a] v)
+        (update :ip inc))))
 
 (defn jnz-op
   [state a b]
@@ -74,17 +94,53 @@
               :set (set-op state a b)
               :sub (sub-op state a b)
               :mul (mul-op state a b)
+              :mod (mod-op state a b)
+              :sqrt (sqrt-op state a b)
               :jnz (jnz-op state a b)))))
 
 (defn simulate
-  [code]
+  [code initial]
   (letfn [(f [state]
             (when state
               (lazy-seq (cons state
                               (f (step code state))))))]
-    (f {:ip 0})))
+    (f initial)))
 
-
-(->> (simulate code)
+(->> (simulate (read-code) {:ip 0})
      (last)
      (println))
+
+(defn dividers
+  [primes n]
+  (take-while #(<= (* % %) n) primes))
+
+(defn prime?
+  [primes n]
+  (every? #(pos? (rem n %))
+          (dividers primes n)))
+
+(defn range-peek
+  [coll]
+  (iterate inc (-> coll peek inc)))
+
+(defn sieve
+  ([] (cons 2 (lazy-seq (sieve [2]))))
+  ([primes]
+   (let [p (->> primes
+                range-peek
+                (filter (partial prime? primes))
+                first)]
+     (cons p (lazy-seq (sieve (conj primes p)))))))
+
+(def b 106500)
+(def c 123500)
+(def primes (take-while #(< % (Math/sqrt c)) (sieve)))
+
+
+(defn fast-algorithm
+  [b c]
+  (->> (range b (inc c) 17)
+       (filter #(not (prime? primes %)))
+       (count)))
+
+(fast-algorithm b c)
